9e0d36f9da2d7ff1d881ae60abb0a29ddbac2dd1
Doni-Metal <bakury94.ldtc@gmail.com>
2022-09-23 18:07:45 -0500

    Implementaciones de es13

---
+++ src/es13/00-at.js
@@ -0,0 +1,7 @@
+const array = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+console.log(array[array.length - 1]); // a la vieja usanza
+console.log(array.at(1)); // con at
+console.log(array.at(0)); // con at
+console.log(array.at(-1)); // con at
+console.log(array.at(-2)); // con at
